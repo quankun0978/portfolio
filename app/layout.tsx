e3efc13f6ecb449ef0e3f4de59bbefb33c54dbb4
quankun0978 <quankun0978@gmail.com>
2024-12-28 16:13:16 +0700

    remove pacakge

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
 import ScrollToTop from "@/components/Helper/ScrollToTop";
-import AnimatedCursor from "react-animated-cursor"
 
 const font = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -22,17 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="custom-scrollbar"> 
+    <html lang="en" className="custom-scrollbar">
       <body
         className={font.className}
       >
-        <div className="hidden md:block">
-          <AnimatedCursor innerSize={8} outerSize={35} innerScale={2} outerScale={2} outerAlpha={0} innerStyle={{
-            border: "3px solid white"
-          }} outerStyle={{
-            border: "3px solid white"
-          }} />
-        </div>
         <ResponsiveNav />
         {children}
         <Footer />
