44a32589f8f6fb5eda86f00778c8918da3322ae0
quankun0978 <quankun0978@gmail.com>
2024-12-28 16:17:35 +0700

    fix error services

--- components/Home/Service/Service.tsx
+++ components/Home/Service/Service.tsx
@@ -9,7 +9,7 @@ const Service = () => {
         <div className='pt-16 pb-16 bg-[#0f0715]'>
             <SectionHeading>Services</SectionHeading>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[80%] mx-auto items-center mt-20'>
-                {servicesData.map((service: IService, i: Number) => {
+                {servicesData.map((service: IService, i) => {
                     return <div data-aos="fade-left" data-aos-anchor-placement="top-center" data-aos-delay={`${i * 150}`} key={service.id}>
                         <ServiceCard service={service} />
                     </div>
